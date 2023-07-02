07bdc155d813fc5c75c3f481a24ddc65e884c698
Priyanka Pakhale <priyanka.pakhale54@gmail.com>
2023-07-02 10:02:36 +0530

    1. added cart icon in assets 2. added header with searchbox and cart icon for filtering and showing borrowed books

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,17 +1,44 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
 import Book from '../../components/Book/Book';
 import styles from './HomePage.module.css';
 import { useSelector } from 'react-redux';
-import { isEmpty, map } from 'lodash';
+import { isEmpty, map, filter, includes, toLower, size } from 'lodash';
+import cartIcon from '../../assets/icons/cart.svg';
 
 const HomePage = () => {
-    const { allBooks } = useSelector(state => state);
+    const { allBooks, borrowedBooks } = useSelector(state => state);
+    const [searchText, setSearchText] = useState('');
+
+    const filteredBooks = useMemo(() => {
+        const query = toLower(searchText);
+        return filter(allBooks, book => {
+            const { name, author, genre } = book;
+            return includes(toLower(name), query) || includes(toLower(author), query) || includes(toLower(genre), query);
+        })
+    }, [allBooks, searchText])
+
+    const handleChange = (e) => {
+        setSearchText(e.target.value);
+    }
+
     return (
         <>
+            <div className={styles.header}>
+                <div className={styles.title}>Rent a book</div>
+                <div className={styles.rightContainer}>
+                    <div className={styles.searchbox}>
+                        <input type="text" placeholder='Type title, author, genre' onChange={handleChange} value={searchText} />
+                    </div>
+                    <div className={styles.cartWrapper}>
+                        {size(borrowedBooks) > 0 && <div className={styles.count}>{size(borrowedBooks)}</div>}
+                        <img className={styles.cartIcon} src={cartIcon} alt="cart" />
+                    </div>
+                </div>
+            </div>
             <div className={styles.wrapper}>
                 <div className={styles.booksWrapper}>
-                    {!isEmpty(allBooks) ?
-                        map(allBooks, book => (
+                    {!isEmpty(filteredBooks) ?
+                        map(filteredBooks, book => (
                             <Book key={book.id} {...book} />
                         )) :
                         <div className={styles.emptyBooksWrapper}>No matching books found</div>}
